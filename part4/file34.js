6b59c8c23eaa1390a74bb09ccdf1fb20b935c0d5
Anish Kumar <anish.kumar232@lpu.in>
2026-02-16 10:51:09 +0530

    updated file34.js

---
+++ part4/file34.js
@@ -0,0 +1,12 @@
+function response(user){
+    console.log(user.name)
+    console.log(user.email)
+}
+const user={
+    name:"John",
+    email:"John@email.com",
+    role:"user",
+
+};
+
+response(user)
