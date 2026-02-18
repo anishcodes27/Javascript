eb65f3db432f9ceca9b87ff10efffcc6ff00596e
Anish Kumar <anish.kumar232@lpu.in>
2026-02-18 12:55:26 +0530

    another files are added

---
+++ part6/file59.js
@@ -0,0 +1,19 @@
+// student = {
+//     name:"Nikhil",
+//     display:function(){
+//         console.log(this.name)
+//     }
+// }
+// student.display()
+
+
+student = {
+    name:"Nikhil",
+    display:()=>{
+        console.log(this.name);
+    },
+};
+student.display();
+
+
+//this all are undefined codes
